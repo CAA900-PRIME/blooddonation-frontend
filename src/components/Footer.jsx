760f4dc7a2fc303ad3796f8b2fcc720a060cee68
ajaysharma2053 <ajaysharma2053@gmail.com>
2025-03-18 17:49:40 +0000

    making footer sticky

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-white text-center p-3 mt-auto">
+        <footer className="bg-dark text-white text-center p-3 mt-auto fixed-bottom w-100">
             <div className="container">
                 <p className="mb-1">&copy; 2025 CAA900-PRIME. All Rights Reserved.</p>
                 <p className="mb-1">Empowering Collaboration, Innovation, and Excellence.</p>
@@ -13,7 +13,6 @@ const Footer = () => {
                     <a href="#" className="text-white">Omar BaGunaid</a>
                     <a href="#" className="text-white">pratima-121</a>
                     <a href="#" className="text-white">rgaraween</a>
-                    <a href="#" className="text-white">Maziar</a>
                 </div>
                 <p className="mt-2">Bringing Together Passionate Individuals to Drive Meaningful Impact.</p>
             </div>
