f8c4baec5cfadbe53c68713fa8139e8aa85983a1
ajaysharma2053 <ajaysharma2053@gmail.com>
2025-03-20 15:26:20 +0000

    created new forgot password page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,23 +6,25 @@ import Dashboard from "../pages/Dashboard";
 import InfoPage from "../pages/InfoPage";
 import BloodRequest from "../pages/BloodRequest";
 import Profile from "../pages/Profile";
+import ForgotPassword from "../pages/forgot-password";
 
 const AppRoutes = ({ user, setUser }) => {
-	const isDev = process.env.NODE_ENV === "development";
+    const isDev = process.env.NODE_ENV === "development";
 
-	return (
-		<Routes>
-			<Route path="/login" element={<Login setUser={setUser} />} />
-			<Route path="/signup" element={<Signup />} />
-			{/* Ensure to redirect user to login if they aren't logged in, unless in development mode */}
-			<Route path="/dashboard" element={isDev || user ? <Dashboard user={user} /> : <Login setUser={setUser} />} />
-			<Route path="/info" element={isDev || user ? <InfoPage user={user} /> : <Login setUser={setUser} />} />
-			<Route path="/home" element={<Home />} />
-			<Route path="/blood-request" element={isDev || user ? <BloodRequest user={user} /> : <Login setUser={setUser} />} />
-			<Route path="/profile" element={isDev || user ? <Profile user={user} /> : <Login setUser={setUser} />} />
-			<Route path="/" element={<Home />} />
-		</Routes>
-	);
+    return (
+        <Routes>
+            <Route path="/login" element={<Login setUser={setUser} />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            {/* Ensure to redirect user to login if they aren't logged in, unless in development mode */}
+            <Route path="/dashboard" element={isDev || user ? <Dashboard user={user} /> : <Login setUser={setUser} />} />
+            <Route path="/info" element={isDev || user ? <InfoPage user={user} /> : <Login setUser={setUser} />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/blood-request" element={isDev || user ? <BloodRequest user={user} /> : <Login setUser={setUser} />} />
+            <Route path="/profile" element={isDev || user ? <Profile user={user} /> : <Login setUser={setUser} />} />
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
 };
 
 export default AppRoutes;
